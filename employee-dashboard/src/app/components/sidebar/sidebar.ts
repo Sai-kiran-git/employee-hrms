c8410dd4c36d09ae6e572c00e0d1e38981e837c1
Sai Kiran <kiranpasupuleti10@gmail.com>
2026-06-03 18:20:07 +0530

    changes

--- employee-dashboard/src/app/components/sidebar/sidebar.ts
+++ employee-dashboard/src/app/components/sidebar/sidebar.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive,MatIconModule],
+  imports: [RouterLink, RouterLinkActive, MatIconModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  constructor(private router: Router) { }
 
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+  
 }
