a96e69dedf3f94d40f646b7335ad40de52b8c23a
Sai Kiran <kiranpasupuleti10@gmail.com>
2026-06-04 10:13:25 +0530

    changes

--- employee-management-backend/src/app.ts
+++ employee-management-backend/src/app.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import authRoutes from './modules/auth/auth.route';
 import employeeRoutes from './modules/employee/employee.route';
 import attendanceRoutes from './modules/attendance/attendance.route';
 import payrollRoutes from './modules/payroll/payroll.route';
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:4200', // Angular frontend
+  credentials: true
+}));
 
 app.use(express.json());
 
